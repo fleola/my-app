084021c0e5400c64e3a9d5b87d08d601233a2340
leo_feola <feola.leopoldo@gmail.com>
2022-05-31 11:23:57 +0200

    components 01

---
+++ components-01/README.md
@@ -0,0 +1,3 @@
+##Components - 01
+
+Create a new React application with create-react-app and remove all the code within the src folder.
--- src/index.js
+++
@@ -1,6 +0,0 @@
-// hello world esempio lezioni
-class HelloWorld extends React.Component{
-    render(){
-        return <h1>Hello, world</h1>
-    }
-}
