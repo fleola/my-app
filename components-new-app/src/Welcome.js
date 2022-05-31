e4fdfe730a07cb77eeb236723a589674ea220845
leo_feola <feola.leopoldo@gmail.com>
2022-05-31 17:20:32 +0200

    conditional rendering 02

--- components-new-app/src/Welcome.js
+++ components-new-app/src/Welcome.js
@@ -6,12 +6,12 @@ export class Welcome extends React.Component{
         return (
         <div>
             <p>Welcome, {this.props.name}!</p>
-            {this.props.age > 18 && <Age age={this.props.age}/>}
+            {!!this.props.age && <Age age={this.props.age}/>}
         </div>
         )
     }
 }
-Welcome.defaultProps={
+/* Welcome.defaultProps={
     name: "user",
     age: "non presente"
-}
+} */
