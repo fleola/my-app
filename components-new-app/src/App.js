571015aca0460242c87399e894818564ad4d25b2
leo_feola <feola.leopoldo@gmail.com>
2022-05-31 16:47:13 +0200

    props 03

--- components-new-app/src/App.js
+++ components-new-app/src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component{
         return (
             <div>
                 <Hello/>
-                <Welcome />
+                <Welcome name="Leo" age="24"/>
             </div>
         )
     }
