352791ec778955f02577a95ad56d3004ce2abb37
leo_feola <feola.leopoldo@gmail.com>
2022-06-03 15:18:53 +0200

    forms 02 fix

--- components-new-app/src/Login.js
+++ components-new-app/src/Login.js
@@ -21,17 +21,23 @@ export class Login extends React.Component {
     return (
       <div>
         <label>Username</label>
-        <input name="username" onChange={this.handleInput}></input>
+        <input
+          name="username"
+          value={this.state.username}
+          onChange={this.handleInput}
+        ></input>
         <label>Password</label>
         <input
           name="password"
           type="password"
+          value={this.state.password}
           onChange={this.handleInput}
         ></input>
         <label>Remember ?</label>
         <input
           name="remember"
           type="checkbox"
+          checked={this.state.remember}
           onChange={this.handleInput}
         ></input>
       </div>
