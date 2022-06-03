fa9fc5e98dee5c09e0df22900349494126196072
leo_feola <feola.leopoldo@gmail.com>
2022-06-03 13:02:33 +0200

    forms 02

---
+++ components-new-app/src/Login.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+export class Login extends React.Component {
+  state = {
+    username: "",
+    password: "",
+    remember: false,
+  };
+  handleInput = (event) => {
+    const name = event.target.name;
+    const type = event.target.type;
+    const value = event.target.value;
+    const remember = event.target.checked;
+
+    this.setState({
+      [name]: type === "checkbox" ? remember : value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <label>Username</label>
+        <input name="username" onChange={this.handleInput}></input>
+        <label>Password</label>
+        <input
+          name="password"
+          type="password"
+          onChange={this.handleInput}
+        ></input>
+        <label>Remember ?</label>
+        <input
+          name="remember"
+          type="checkbox"
+          onChange={this.handleInput}
+        ></input>
+      </div>
+    );
+  }
+}
