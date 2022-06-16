c3d0d8cd556ca6c10340b12881ac4b66a350ff08
leo_feola <feola.leopoldo@gmail.com>
2022-06-16 20:15:55 +0200

    useState-01

--- components-new-app/README.md
+++ components-new-app/README.md
@@ -1,3 +1,3 @@
-## Events - 01
+## useState - 01
 
-Create a ClickCounter class component that increments a counter every time a user clicks on a button. Render both the current value of the counter and the button within the ClickCounter component.
+Rewrite the Counter component from Events 01 as a function component, and use the useState hook to track the state of the counter.
--- components-new-app/src/Counter.js
+++ components-new-app/src/Counter.js
@@ -1,23 +1,10 @@
-import React from "react";
+import { useState } from "react";
 import { CounterDisplay } from "./CounterDisplay";
 
-export class Counter extends React.Component{
-    state={
-        count: this.props.initialCount
-    }
-    componentDidMount(){
-        setInterval(()=>{
-            this.setState((state)=>{
-               return {count: state.count +this.props.incrementAmount}
-            })  
-        },this.props.incrementInterval)
-    }
-    render(){
-        return <CounterDisplay count={this.state.count}/>
-    }
-}
-Counter.defaultProps={
-    initialCount: 0,
-    incrementInterval: 1000,
-    incrementAmount: 1,
+export function Counter(props) {
+  const [count, setCount] = useState(props.initialCount);
+  setInterval(() => {
+    setCount(count + props.incrementAmount);
+  }, props.incrementInterval);
+  return <CounterDisplay count={count} />;
 }
