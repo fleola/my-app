dffbe295691efbba7687e2a774a4322c92ae3f8a
leo_feola <feola.leopoldo@gmail.com>
2022-06-17 13:01:02 +0200

    useEffect-02

--- components-new-app/src/Counter.js
+++ components-new-app/src/Counter.js
@@ -1,17 +1,17 @@
-import React from "react";
-export class Counter extends React.Component{
-    state={
-        count:0,
-    }
-    constructor(props){
-        super(props)
-        setInterval(()=>{
-            this.setState((state)=>{
-               return {count: state.count +1}
-            })  
-        },1000)
-    }
-    render(){
-        return <h1>Counter: {this.state.count}</h1>
-    }
+import { useEffect, useState } from "react";
+
+export function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <h1>Counter: {count}</h1>;
 }
