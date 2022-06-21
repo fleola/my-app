66c2046c54c012d335cb754065436d7d32223503
leo_feola <feola.leopoldo@gmail.com>
2022-06-21 11:03:31 +0200

    react-router-02

--- components-new-app/src/App.js
+++ components-new-app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Welcome } from "./Welcome";
 import { Route, Routes } from "react-router-dom";
+import { ClickCounter } from "./ClickCounter";
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Welcome name="Leo" />} />
+      <Route path="/counter" element={<ClickCounter />} />
     </Routes>
   );
 }
